881a0e64226e828da862c4f32f76f8ab50efc0e3
idaefarinaa <151916890+idaefarinaa@users.noreply.github.com>
2023-12-01 16:35:11 +0700

    Add files via upload

---
+++ idaefarinalist.py
@@ -0,0 +1,5 @@
+daftar_nama = ["andika", "amelani", "verliza", "salman", "zebian"]
+daftar_nilai_pplg1 = [80, 90, 100, 95, 90, 80]
+
+jumlah = len(daftar_nama) # menegtahui jumlah data list
+               
